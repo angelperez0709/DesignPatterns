f7ccc01efba5fa8e017dcb128e82d5d2cc6cb82d
Ángel Pérez Martínez <angelperez07092000@gmail.com>
2025-02-07 20:47:39 +0100

    Starting builder

---
+++ src/Creational/Builder.java
@@ -0,0 +1,8 @@
+package Creational;
+
+//Excellent choice for constructing complex objects with diverse configuration options.
+interface BuilderInterface{
+
+}
+public class Builder  implements BuilderInterface{
+}
